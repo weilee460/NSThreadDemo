59ca8c6e0bc88d53fc110125b76b2e55ea16063e
Eagle <wei.lee460@gmail.com>
2016-04-08 13:00:04 +0800

    Add NSThread implemention.  2016-04-08

--- NSThreadDemo/ViewController.swift
+++ NSThreadDemo/ViewController.swift
@@ -9,16 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //定义两个线程
+    var thread1: NSThread?
+    var thread2: NSThread?
+    
+    //定义两个线程条件，用于锁住线程
+    let condition1 = NSCondition()
+    let condition2 = NSCondition()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        thread1 = NSThread(target: self, selector: "method1:", object: nil)
+        thread2 = NSThread(target: self, selector: "method2:", object: nil)
+        thread1?.start()
+    }
+    
+    func method1(sender: AnyObject)
+    {
+        for var i = 0; i < 10; i++
+        {
+            print("NSThread 1 running \(i)")
+            sleep(1)
+            
+            if i == 2
+            {
+                thread2?.start()
+                //本线程thread1锁定
+                condition1.lock()
+                condition1.wait()
+                condition1.unlock()
+            }
+        }
+        
+        print("NSThread 1 over")
+        
+        //线程2激活
+        condition2.signal()
+    }
+    
+    func method2(sender: AnyObject)
+    {
+        for var i = 0; i < 10; i++
+        {
+            print("NSThread 2 running \(i)")
+            sleep(1)
+            
+            if i == 2
+            {
+                //线程1激活
+                condition1.signal()
+                
+                //线程2锁定
+                condition2.lock()
+                condition2.wait()
+                condition2.unlock()
+            }
+        }
+        
+        print("NSThread 2 over")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
